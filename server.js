df0f3720e1ff576251601d50431f379e1bbf614e
Blair Tingey <btingey@salesforce.com>
2021-07-23 20:59:02 -0600

    update path to correct app name

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-reddit'));
+app.use(express.static(__dirname + '/dist/angualr-reddit'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/angular-reddit/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angualr-reddit/index.html'));
 
 });
 
