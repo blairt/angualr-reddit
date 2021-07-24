cb4bde64a9a1fa0623dcd424af9e41eb78d81c16
Blair Tingey <btingey@salesforce.com>
2021-07-23 20:31:30 -0600

    path changes

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist/angular-reddit'));
 
 app.get('/*', function(req,res) {
-    
 res.sendFile(path.join(__dirname+'/dist/angular-reddit/index.html'));
+
 });
 
 // Start the app by listening on the default Heroku port
